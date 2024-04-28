2397652bcaafb7b4163ad40226906f9f79d6be8d
Unknown2k10 <emmanuelokafor39@gmail.com>
2024-04-28 23:23:24 +0100

    new update

---
+++ public/emptyCart.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empty Shopping Cart</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <style>
+        /* Add your custom styles here */
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Empty Shopping Cart</h1>
+    <p>There are no items in your shopping cart.</p>
+    <a href="../src/functions.php" class="btn btn-primary">Back to Products</a>
+</div>
+</body>
+</html>
